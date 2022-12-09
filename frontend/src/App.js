f7344aee5fa509ad75cee19a988814148749a845
murraydeam <73445952+murraydeam@users.noreply.github.com>
2022-12-09 13:49:33 -0500

    Initial Commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,8 @@
 import './App.css';
-import { useEffect } from 'react'
-
 
 function App() {
-useEffect(() => {
-  document.title = 'Dash Auto'
-
-
-}, [])
-
-
   return (
-    
-    <div className="mainContainer">
+    <div className="App">
       
     </div>
   );
